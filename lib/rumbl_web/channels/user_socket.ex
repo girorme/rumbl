6cb155c1f89f7200fd322070155bdec015396261
girorme <rodrigogirorme@gmail.com>
2023-03-10 20:22:08 -0300

    improve video socket connect and add verified user token logic

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -3,9 +3,22 @@ defmodule RumblWeb.UserSocket do
 
   channel "videos:*", RumblWeb.VideoChannel
 
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  @max_age 2 * 7 * 24 * 60 * 60
+
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(
+      socket,
+      "user socket",
+      token,
+      max_age: @max_age
+    ) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
 
-  def id(_socket), do: nil
+  def connect(_params, _socket, _connect_info), do: :error
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 end
