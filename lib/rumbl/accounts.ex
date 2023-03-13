51f444388507a44cfa8f09f0bc77f2af0da46056
girorme <rodrigogirorme@gmail.com>
2023-03-12 21:29:43 -0300

    add presence tracking

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,7 +2,7 @@ defmodule Rumbl.Accounts do
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  import Ecto.Changeset
+  import Ecto.{Changeset, Query}
 
   def get_user(id) do
     Repo.get(User, id)
@@ -66,5 +66,9 @@ defmodule Rumbl.Accounts do
         Pbkdf2.no_user_verify()
         {:error, :not_found}
     end
+  end
+
+  def list_users_with_ids(ids) do
+    Repo.all(from(u in User, where: u.id in ^ids))
   end
 end
